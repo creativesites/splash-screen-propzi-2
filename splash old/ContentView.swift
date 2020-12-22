015c5efa2520561ee08e0b15beb7f11f36e72044
Winston Chikazhe <creativesites263@gmail.com>
2020-12-22 18:04:29 +0200

    Initial Commit

---
+++ splash old/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  splash old
+//
+//  Created by Winston T Chikazhe on 22/12/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ splash old/splash_oldApp.swift
@@ -0,0 +1,17 @@
+//
+//  splash_oldApp.swift
+//  splash old
+//
+//  Created by Winston T Chikazhe on 22/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct splash_oldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
